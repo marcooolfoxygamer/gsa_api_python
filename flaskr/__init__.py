8574d111df7188ce07a66467b8d24664153c29f5
Mariana <mmarinrojas97@gmail.com>
2023-09-01 11:07:57 -0500

    Se agregan cambios con respecto a la migración de las tablas de la base de datos al modelo creado para este propósito

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -5,8 +5,8 @@ from flask_sqlalchemy import SQLAlchemy
 def create_app(config_name):
     app = Flask(__name__)
     USER_DB = 'root'
-    PASS_DB = 'marco'
-    URL_DB = 'localhost:3307'
+    PASS_DB = 'sena'
+    URL_DB = 'localhost'
     NAME_DB = 'bd_gymsenapp'
     FULL_URL_DB = f'mysql://{USER_DB}:{PASS_DB}@{URL_DB}/{NAME_DB}'
 
